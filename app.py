2a6647eaa41c4b578251610b6143f7eb846fb0ba
shivalik-akarozi <113537497+shivalik-akarozi@users.noreply.github.com>
2023-04-24 23:15:12 +0530

    added flask and html gui components

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+import cv2
+import numpy as np
+import cv2
+
+from recolor import Core
+
+
+
+
+
+
+
+
+app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/after', methods=['GET', 'POST'])
+def after():
+
+    global model, resnet, vocab, inv_vocab
+
+    img = request.files['file1']
+
+    img.save('static/file.jpg')
+
+
+
+    
+    image = cv2.imread('static/file.jpg')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    image = cv2.resize(image, (224,224))
+    
+    image = np.reshape(image, (1,224,224,3))
+    Core.correct(input_path='static/file.jpg',
+                 return_type='save',
+                 save_path='static/file1.jpg',
+                 protanopia_degree=0.9,
+                 deutranopia_degree=0.0)
+
+    return render_template('after.html', data="hello")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
